d77a2f18ae54c83de33d2a3eccd4bae687e60db6
Ayda <104395137+aydozy@users.noreply.github.com>
2024-03-10 18:00:13 +0300

    Update button_decoration.dart

--- lib/components/button_decoration.dart
+++ lib/components/button_decoration.dart
@@ -8,16 +8,12 @@ class MyCustomButtonStyle {
   static ButtonStyle get customStyle {
     return ElevatedButton.styleFrom(
       onPrimary: Color(0xffE85227),
-      // Text color
       primary: Colors.white,
-      // Background color
       minimumSize: Size(100, 36),
-      // Minimum size
       padding: EdgeInsets.symmetric(horizontal: 16),
-      // Inner padding
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(10)),
-        side: BorderSide(color: Color(0xffE85227)), // Rounded corners
+        side: BorderSide(color: Color(0xffE85227)), 
       ),
     );
   }
@@ -25,16 +21,12 @@ class MyCustomButtonStyle {
   static ButtonStyle get customStyle2 {
     return ElevatedButton.styleFrom(
       onPrimary: Colors.white,
-      // Text color
       primary: Color(0xff4EBBB4),
-      // Background color
       minimumSize: Size(100, 36),
-      // Minimum size
       padding: EdgeInsets.symmetric(horizontal: 16),
-      // Inner padding
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(10)),
-        side: BorderSide(color: Color(0xff4EBBB4)), // Rounded corners
+        side: BorderSide(color: Color(0xff4EBBB4)),
       ),
     );
   }
