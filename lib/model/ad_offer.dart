725b93f1fe5bc37fa3e658a0e9e9fb3902d264c3
Ayda <104395137+aydozy@users.noreply.github.com>
2024-03-10 18:01:48 +0300

    Update ad_offer.dart

--- lib/model/ad_offer.dart
+++ lib/model/ad_offer.dart
@@ -1,6 +1,3 @@
-//AdOffer, represents a model for an advertising offer,
-// encapsulating various details about the advertisement
-
 class AdOffer {
   final String adButtonText;
   final String adContent;
@@ -17,10 +14,6 @@ class AdOffer {
     required this.bank,
     required this.bankId,
   });
-
-  //The constructor "AdOffer.fromJson", which is a factory constructor that creates an instance of AdOffer from a JSON object.
-  // This factory constructor takes a Map<String, dynamic> (representing JSON data) as an input,
-  // extracts the relevant fields using the keys (e.g., 'ad_button_text', 'ad_content', etc.).
 
   factory AdOffer.fromJson(Map<String, dynamic> json) {
     return AdOffer(
